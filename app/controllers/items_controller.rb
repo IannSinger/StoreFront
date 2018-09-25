e4b40e2723c82a7c0ce45a818180fc2dba756d70
LampshadeWarrior <itpsinger@gmail.com>
2018-09-24 23:07:00 -0400

    Add item controller with index and new functions

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,21 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    @item.save
+    redirect_to items_path
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:name, :price)
+    end
+end
